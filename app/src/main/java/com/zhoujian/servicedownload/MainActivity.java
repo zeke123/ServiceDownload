57d7cbc34759f62b6e4b3944757545ebe8f6afc9
zeke123 <zekezhoujian@163.com>
2017-03-27 09:59:49 +0800

    解决Android7.0无法安装的问题

--- app/src/main/java/com/zhoujian/servicedownload/MainActivity.java
+++ app/src/main/java/com/zhoujian/servicedownload/MainActivity.java
@@ -72,6 +72,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         switch (requestCode) {
